9a4af3606b568a5a5b27ca00faddd7aae80e1775
Kovshik387 <fantokin03@mail.ru>
2023-12-11 16:59:21 +0300

    init

--- src/main/java/com/example/programmingtechnologyprototip/Controller.java
+++ src/main/java/com/example/programmingtechnologyprototip/Controller.java
@@ -8,7 +8,6 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.input.MouseEvent;
